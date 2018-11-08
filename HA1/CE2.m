b0765f75a4993bd4893f95c8bf88ad3b3b8a92ff
TheoBerglin <theoberglin@outlook.com>
2018-11-08 09:33:35 +0100

    Plotting of lines work. Plotting of points not working prop. Intersection calculated

--- HA1/CE2.m
+++ HA1/CE2.m
@@ -1,6 +1,7 @@
 clear all, close all, clc
 im = imread('compEx2.jpg');
 
+im_dim = size(im);
 imagesc(im)
 colormap gray
 
@@ -12,20 +13,24 @@ p2_cart = pflat(p2);
 p3_cart = pflat(p3);
 
 % Plot the points
-pointPlot(p1_cart)
-pointPlot(p2_cart)
-pointPlot(p3_cart)
+pointPlot(p1_cart, im_dim);
+pointPlot(p2_cart, im_dim);
+pointPlot(p3_cart, im_dim);
 
 % Calculate lines through points
 l1 = lineFromPoints(p1_cart);
-l2 = lineFromPoints(p1_cart);
-l3 = lineFromPoints(p1_cart);
+l2 = lineFromPoints(p2_cart);
+l3 = lineFromPoints(p3_cart);
 
 % Plot the lines
-rital(l1, 1)
-rital(l2, 1)
-rital(l3, 1)
+rital(l1)
+rital(l2)
+rital(l3)
 
+% Compute point of intersection between l2 and l3
+L = [l2, l3];
+I = null(L');
+I_cart = pflat(I);
 
 
 
@@ -38,6 +43,6 @@ coefficients = [k -1 m]';
 %line_coeff = polyfit([points_cart(1,1), points_cart(1,2)], [points_cart(2,1), points_cart(2,2)], 1);
 %coefficients = [1/line_coeff(2), -line_coeff(1)/line_coeff(2), +1]';
 end
-function pointPlot(points)
+function pointPlot(points, im_dim)
 plot(points(:,1), points(:,2),'x');
 end
