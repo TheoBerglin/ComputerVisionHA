0142d29b9cb1e8a95af71289b55dee3517f90fb2
TheoBerglin <theoberglin@outlook.com>
2018-11-08 09:46:57 +0100

    Done with computer exercise 2

--- HA1/CE2.m
+++ HA1/CE2.m
@@ -13,9 +13,9 @@ p2_cart = pflat(p2);
 p3_cart = pflat(p3);
 
 % Plot the points
-pointPlot(p1_cart, im_dim);
-pointPlot(p2_cart, im_dim);
-pointPlot(p3_cart, im_dim);
+pointPlot(p1_cart');
+pointPlot(p2_cart');
+pointPlot(p3_cart');
 
 % Calculate lines through points
 l1 = lineFromPoints(p1_cart);
@@ -32,7 +32,19 @@ L = [l2, l3];
 I = null(L');
 I_cart = pflat(I);
 
+% Plot intersection point
+pointPlot(I_cart')
 
+% Calculate distance between line 1 and intersection point l2 and l3
+distance = calculateDistanceLinePoint(l1, I_cart);
+fprintf('The distance between line 1 and intersection point l2-l3 is: %.2f\n', distance);
+export_fig('Results/CE_2.pdf', '-pdf','-transparent');
+
+function distance = calculateDistanceLinePoint(line, point)
+num = line'*[point; 1];
+denum = line(1)^2+line(2)^2;
+distance = abs(num)/sqrt(denum);
+end
 
 function coefficients = lineFromPoints(points_cart)
 % returns [a b c]' as ax+by+c=0
@@ -43,6 +55,6 @@ coefficients = [k -1 m]';
 %line_coeff = polyfit([points_cart(1,1), points_cart(1,2)], [points_cart(2,1), points_cart(2,2)], 1);
 %coefficients = [1/line_coeff(2), -line_coeff(1)/line_coeff(2), +1]';
 end
-function pointPlot(points, im_dim)
-plot(points(:,1), points(:,2),'x');
+function pointPlot(points)
+plot(points(:,1), points(:,2),'x','LineWidth',2,'MarkerSize',10, 'color', 'red');
 end
